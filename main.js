61e520fe3a46a6b242321f8b8b2c84470964413f
DavidMayorgaS08 <david08102@hotmail.com>
2024-06-24 10:23:11 -0500

    cors

--- main.js
+++ main.js
@@ -21,9 +21,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: '*',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization']
+        origin: 'http://localhost:5173',
     }
 ));
 app.use(express.static('public'))
